a5d21b0f5a9360d0f31c22491c0e08cd15bee035
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2024-06-21 19:48:30 +0100

    Update CombatData.cs

--- PathToApocalypse/Assets/Scripts/CombatData.cs
+++ PathToApocalypse/Assets/Scripts/CombatData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewCombatData", menuName = "Combat Data", order = 51)]
 public class CombatData : ScriptableObject
 {
-    public int difficulty;
+    public Difficulty difficulty;
     public RewardType rewardType;
     public Need rewardNeed; // Optional parameter for a need
     public GameObject rewardPrefab;  // Optional parameter for a prefab
@@ -20,4 +20,11 @@ public enum Need
     Hunger,
     Thirst,
     Health,
+}
+
+public enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard,
 }
